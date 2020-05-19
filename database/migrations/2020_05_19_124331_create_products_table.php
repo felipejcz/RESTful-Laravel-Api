d3b35c26082db54045dc665bd7cffe354eb6cc72
Felipe Zamboni <felipejcz@gmail.com>
2020-05-19 09:58:03 -0300

    Add Model,Migration(Category,Product)

---
+++ database/migrations/2020_05_19_124331_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->text('description');
+            $table->float('price');
+            $table->integer('owner')->unsigned();
+            $table->foreign('owner')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
